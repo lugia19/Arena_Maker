51bf90547a713c39a527740740ef22db653d678f
lugia19 <lugia19@lugia19.com>
2024-08-08 13:27:55 +0200

    Add compilation error messagebox

--- gui.py
+++ gui.py
@@ -55,12 +55,12 @@ class PathWidget(QWidget):
 class Worker(QObject):
     finished = pyqtSignal()
     progress = pyqtSignal(int, str)
-
+    error = pyqtSignal(str)
     def run(self):
-        #try:
-        core.compile_folder(self.progress)
-        #except Exception as e:
-            #QMessageBox.critical(None, "Error", f"Compilation failed:\n\n{e}")
+        try:
+            core.compile_folder(self.progress)
+        except Exception as e:
+            self.error.emit(f"Compilation failed:\n\n{e}")
 
         self.finished.emit()
 
@@ -88,9 +88,12 @@ class ProgressDialog(QDialog):
         self.thread.finished.connect(self.thread.deleteLater)
         self.worker.progress.connect(self.update_progress)
         self.worker.finished.connect(self.accept)
+        self.worker.error.connect(self.error_display)
 
     def start_task(self):
         self.thread.start()
+    def error_display(self, error):
+        QMessageBox.critical(None, "Error", error)
     def closeEvent(self, event) -> None:
         event.ignore()
 
